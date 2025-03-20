fab4150ab716be5918ceacc8bcc053bad11b5ba3
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-03-20 13:50:28 +0500

    fix issue

--- components/svgs/mice-svg.js
+++ components/svgs/mice-svg.js
@@ -1,4 +1,4 @@
-const MiceSvg = (props: any) => {
+const MiceSvg = () => {
   return (
     <svg
       id="Layer_1"
@@ -6,10 +6,9 @@ const MiceSvg = (props: any) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 43.18 78.92"
       className="w-[19.58px] lg:w-[27.12px] 3xl:w-[35.25px] h-auto"
-      {...props}
     >
       <path
-        className="cls-1"
+        fill="white" // ✅ Set fill color to white
         d="M21.59,0C9.69,0,0,9.69,0,21.59v35.74c0,11.9,9.69,21.59,21.59,21.59s21.59-9.69,21.59-21.59V21.59C43.18,9.69,33.49,0,21.59,0ZM41.18,57.33c0,10.8-8.79,19.59-19.59,19.59S2,68.13,2,57.33V21.59C2,10.79,10.79,2,21.59,2s19.59,8.79,19.59,19.59v35.74Z"
       />
     </svg>
