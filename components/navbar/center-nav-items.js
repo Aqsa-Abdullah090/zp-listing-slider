08229c5d465e7e25e8ddfb87afd9efc70018c12c
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-03-21 15:41:54 +0500

    fix navbar

--- components/navbar/center-nav-items.js
+++ components/navbar/center-nav-items.js
@@ -1,29 +1,22 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { links } from "./contants";
 import clsx from "clsx";
 
 const CenterNavItems = () => {
-
- 
-
   return (
-    <>
-      <ul className="flex lg:space-x-3 xl:space-x-5 2xl:space-x-[48px] 4xl:space-x-[1vw]">
-        {links.map((link) => (
-          <li
-            key={link}
-            className={clsx(
-              "uppercase text-[6.67px] lg:text-[6.5px] xl:text-[9.23px] 2xl:text-[15px] tracking-[3px] whitespace-nowrap opacity-50",
-            
-            
-            )}
-          >
-            {link}
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className="flex lg:space-x-3 xl:space-x-5 2xl:space-x-[48px] 4xl:space-x-[1vw]">
+      {links.map((link) => (
+        <li
+          key={link}
+          className={clsx(
+            "uppercase text-[6.67px] lg:text-[6.5px] xl:text-[9.23px] 2xl:text-[15px] tracking-[3px] whitespace-nowrap opacity-50 hover:opacity-100 cursor-pointer"
+          )}
+        >
+          {link}
+        </li>
+      ))}
+    </ul>
   );
 };
 
--- components/navbar/right-nav-items.tsx
+++ components/navbar/right-nav-items.tsx
@@ -1,41 +1,46 @@
-"use client";
 import UserSvg from "@/components/svgs/user-svg";
 import ZigProSell from "@/components/svgs/zig-pro-sell-svg";
 import clsx from "clsx";
 import React from "react";
 import { useTheme } from "@/context/theme-context"; // Import theme context
 
-const RightNavItems = ({ variant, expanded = false }) => {
+const RightNavItems = ({
+  variant,
+  expanded = false,
+}: {
+  variant: string;
+  expanded?: boolean;
+}) => {
   const { theme } = useTheme(); // Get theme from context
 
   return (
     <>
-      <div
+      {/* <div
         className={clsx(
           variant === "fixed" && "duration-300 ease-linear",
-          variant === "fixed" && !expanded
+          variant === "fixed" && !expanded 
             ? "absolute z-0 translate-x-[150px]"
-            : expanded
+            : expanded 
             ? "translate-x-0"
-            : ""
+            : "" 
         )}
       >
         <ZigProSell className="fill-inherit" />
-      </div>
+      </div> */}
       {/* Apply theme-based fill color */}
       <UserSvg
         className={clsx(
-          "w-auto h-[16.67px] lg:h-[20.08px] 2xl:h-[30px] flex-shrink-0 z-10",
-          theme === "light" ? "fill-black" : "fill-white"
+          "w-auto h-[16.67px] lg:h-[40.08px] 2xl:h-[30px] flex-shrink-0 z-10",
+          theme === "light" ? "fill-black" : "fill-white" // Change fill color based on theme
         )}
       />
-      
+      <div className="z-10">
         <img
           src="/assets/temp/uk-flag.png"
           alt="uk flag"
-          className="w-[22.22px] lg:w-[30.77px] 2xl:w-[42px] h-auto flex-shrink-0"
+          className="w-[22.22px] lg:w-[42.77px] 2xl:w-[60px] h-auto flex-shrink-0"
         />
-     
+      </div>
     </>
   );
 };
