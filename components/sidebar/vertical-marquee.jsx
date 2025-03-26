db5099ec298cc21ca17958b5eebb4dbaed6da22e
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-03-26 16:26:54 +0500

    fix sidebar

--- components/sidebar/vertical-marquee.jsx
+++ components/sidebar/vertical-marquee.jsx
@@ -157,57 +157,9 @@ const tempThumbnailImages = [
     thumbnail:
       "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F4.png?alt=media&token=ed013751-34d6-46f4-b310-1e04dff34bea",
   },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F1.png?alt=media&token=ad1d9ceb-5f0a-4f46-9a26-f4014c6b6d02",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F2.png?alt=media&token=d1ef2f56-f3be-46cc-80bd-60fdee4bafe6",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F3.png?alt=media&token=f3018214-821d-4182-9728-2e81d234c106",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F4.png?alt=media&token=ed013751-34d6-46f4-b310-1e04dff34bea",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F1.png?alt=media&token=ad1d9ceb-5f0a-4f46-9a26-f4014c6b6d02",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F2.png?alt=media&token=d1ef2f56-f3be-46cc-80bd-60fdee4bafe6",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F3.png?alt=media&token=f3018214-821d-4182-9728-2e81d234c106",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F4.png?alt=media&token=ed013751-34d6-46f4-b310-1e04dff34bea",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F1.png?alt=media&token=ad1d9ceb-5f0a-4f46-9a26-f4014c6b6d02",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F2.png?alt=media&token=d1ef2f56-f3be-46cc-80bd-60fdee4bafe6",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F3.png?alt=media&token=f3018214-821d-4182-9728-2e81d234c106",
-  },
-  {
-    thumbnail:
-      "https://firebasestorage.googleapis.com/v0/b/zimopro-7df3c.appspot.com/o/testing-images%2F400%2F4.png?alt=media&token=ed013751-34d6-46f4-b310-1e04dff34bea",
-  },
 ];
 
-// Duplicate images to ensure seamless scrolling
+// Duplicate images for seamless scrolling
 const scrollingImages = [...tempThumbnailImages, ...tempThumbnailImages];
 
 function VerticalMarquee() {
@@ -215,11 +167,12 @@ function VerticalMarquee() {
     <div className="relative h-[100vh] w-full overflow-hidden">
       <motion.div
         className="flex flex-col space-y-4"
-        animate={{ y: ["-100%", "0%"] }} // Moves up infinitely
+        initial={{ y: 0 }} // Start from the first 4 images visible
+        animate={{ y: ["-100%", "-75%", "-50%", "-25%", "0%"] }} // Moves 1 image at a time
         transition={{
           ease: "linear",
-          duration: 50, // Speed of scrolling
-          repeat: Infinity, // Keeps looping forever
+          duration: 10, // Adjust speed
+          repeat: Infinity,
           repeatType: "loop",
         }}
       >
@@ -256,6 +209,8 @@ function VerticalMarquee() {
 }
 
 export default VerticalMarquee;
+
+
 
 
 
