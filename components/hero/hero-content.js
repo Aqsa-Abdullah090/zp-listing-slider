0b588b5293ad680e76a3f6064999122a5f4242bf
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-03-27 16:05:48 +0500

    fix sidebar

--- components/hero/hero-content.js
+++ components/hero/hero-content.js
@@ -10,18 +10,18 @@ const HeroContent = ({ agent }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
-      className="space-y-[2.08dvh] 2xl:space-y-[2.96dvh] w-[347.22px] lg:w-[480.77px] 3xl:w-[625px] duration-10 transition-all"
+      className="space-y-[2.08dvh] 2xl:space-y-[2.96dvh] w-[347.22px] lg:w-[480.77px] 3xl:w-[625px] duration-1000 transition-all"
     >
       <img
         src={agent?.agent_logo}
         alt={agent?.name}
         className="h-[20px] lg:h-[27.78px] 2xl:h-[38.46px]"
       />
-      <p className="text-white text-[10.00px] lg:text-[12px] 2xl:text-[18px] tracking-[2px] 2xl:tracking-[3.5px] uppercase whitespace-nowrap">
+       <p className="text-white text-[10.00px] lg:text-[13.85px] 2xl:text-[18px] tracking-[3px] uppercase whitespace-nowrap">
         <span dir="ltr">$</span>
         {agent.amount} {agent.currency}
       </p>
-      <div className="overflow-hidden sm:overflow-visible uppercase flex space-x-[16.67px] lg:space-x-[23.08px] 2xl:space-x-[38px] whitespace-nowrap text-[6.67px] lg:text-[6.5px] xl:text-[9.23px] 2xl:text-[12px] text-white tracking-[2px] 2xl:tracking-[3px]">
+      <div className="overflow-hidden sm:overflow-visible uppercase flex space-x-[16.67px] lg:space-x-[23.08px] 2xl:space-x-[32px] whitespace-nowrap text-[6.67px] lg:text-[6.5px] xl:text-[9.23px] 2xl:text-[12px] text-white tracking-[3px]">
         <p>{agent.address_line_1}</p>
         {agent.address_line_2 && <p>{agent.address_line_2}</p>}
         <p>{agent.city}</p>
@@ -29,7 +29,7 @@ const HeroContent = ({ agent }) => {
         <p>{agent.country_name}</p>
       </div>
 
-      <button className="h-[20px] lg:h-[27.69px] 2xl:h-[36px] px-4 2xl:px-6  flex justify-between space-x-4 text-white bg-transparent hover:bg-white hover:text-black transition-all duration-300 border-2 border-white items-center text-[7.78px] lg:text-[10.77px] 2xl:text-[14px] tracking-[1.5px] whitespace-nowrap cursor-pointer">
+      <button className="h-[20px] lg:h-[27.69px] 2xl:h-[36px] px-4 2xl:px-6  flex justify-between space-x-4 text-white bg-transparent hover:bg-white hover:text-black transition-all duration-300 border-2 border-white items-center text-[7.78px] lg:text-[10.77px] 2xl:text-[14px] tracking-[2.5px] whitespace-nowrap cursor-pointer">
         <p>
           <span dir="ltr">$</span>
           25.00
